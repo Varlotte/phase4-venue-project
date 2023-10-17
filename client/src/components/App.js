24d2525905cb1571ee03f6aa54c00a52c4e022d0
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-10-17 14:45:59 -0400

    installs formik, starts creating a test form, proxies request to API

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,31 +5,29 @@ import { Routes, Route } from "react-router-dom";
 import EventsList from "./EventsList";
 
 function App() {
-
   const [eventsList, setEventsList] = useState([]);
-  
+
+  // since we have proxy in our package.json can we just do fetch("/events")?
+  //"http://127.0.0.1:5555/events" Commented out so we can put it back if it doesn't work
+
   useEffect(() => {
-    fetch("http://127.0.0.1:5555/events")
-    .then(r => r.json())
-    .then(events => {
-      setEventsList(events)
-    })
-  }, [])
+    fetch("/events")
+      .then((r) => r.json())
+      .then((events) => {
+        setEventsList(events);
+      });
+  }, []);
 
-  console.log(eventsList)
-
-
-
-
+  console.log(eventsList);
 
   const yo = ["hi"];
 
   return (
-  <div>
-     <h1>Venue Project</h1>
-     <EventsList events={eventsList}/>
-  </div>
-  )
+    <div>
+      <h1>Venue Project</h1>
+      <EventsList events={eventsList} />
+    </div>
+  );
 }
 
 export default App;
