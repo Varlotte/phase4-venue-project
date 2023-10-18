c63d7f2a413d18ed16d10d165bed379ae98fab69
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-10-17 22:18:39 -0400

    implements frontend routing with homepage route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from "react";
 
 //CHANGED Switch to Routes - Sergio 10/17
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import EventsList from "./EventsList";
-import FullEventCard from "./FullEventCard";
+//import Navbar from "./Navbar";
 
 function App() {
   const [eventsList, setEventsList] = useState([]);
-  const [currentEvent, setCurrentEvent] = useState({id: "", name: "", price: "", description: "", event_date: "", time: "", location: "", reservations: " "})
-  // since we have proxy in our package.json can we just do fetch("/events")?
-  //"http://127.0.0.1:5555/events" Commented out so we can put it back if it doesn't work
-  //nvm it did not like that and I had to change it back (c, 10/17)
 
   useEffect(() => {
     fetch("http://127.0.0.1:5555/events")
@@ -20,20 +16,16 @@ function App() {
       });
   }, []);
 
-  function selectEvent(eventID) {
-    console.log(eventID)
-    fetch(`http://127.0.0.1:5555/events/${eventID}`)
-    .then(r => r.json())
-    .then(event => {setCurrentEvent(event)})
-  }
-  
-  
-
   return (
     <div>
-      <h1>Venue Project</h1>
-      <EventsList events={eventsList} selectEvent={selectEvent}/>
-      <FullEventCard selectedEvent={currentEvent}/>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">
+            <h1>Welcome to the Restful!</h1>
+            <EventsList events={eventsList} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
