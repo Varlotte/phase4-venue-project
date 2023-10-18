c0a72ffb160c0966d4fee0844a876eeb10b566d8
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-10-18 16:59:05 -0400

    updating accountdash text for hilarity

--- client/src/layout/AcctDash.js
+++ client/src/layout/AcctDash.js
@@ -6,6 +6,7 @@ function AcctDash() {
       <h1>This is your account dashboard, cool!</h1>
       <h2>we can show your events here</h2>
       <h2>also your account information</h2>
+      <h3>CRUD will happen here, we promise</h3>
     </div>
   );
 }
