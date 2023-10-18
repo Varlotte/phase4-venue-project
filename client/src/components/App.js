2885b767bc01f498c79cde4921a6051ca677a8b9
Idalis Valadez <idalisvaladez22@gmail.com>
2023-10-18 11:47:22 +0300

    layout

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,15 +3,15 @@ import React from "react";
 //CHANGED Switch to Routes - Sergio 10/17
 //changed it back to switch because routes is legacy code and it wasn't running- charlotte 10/17
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Navbar from "./NavBar";
-import Home from "./Home";
+import Navbar from "../layout/NavBar";
+import Home from "../layout/Home";
 import CreateAccount from "./CreateAcct";
 import Login from "./Login";
 
 function App() {
 
   console.log("PLEASEEEEEEEE")
-  
+
   return (
     <div>
       <BrowserRouter>
--- client/src/layout/Home.js
+++ client/src/layout/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import EventsList from "./EventsList";
+import EventsList from "../components/EventsList";
 
 function Home() {
   const [eventsList, setEventsList] = useState([]);
