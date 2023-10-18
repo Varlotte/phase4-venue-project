25633ad0f17c541bb5a71023009ba3e7ff6b0aa2
Idalis Valadez <idalisvaladez22@gmail.com>
2023-10-18 20:50:18 +0300

    Signup form complete, post fetch, css

--- client/src/layout/SignForm.jsx
+++ client/src/layout/SignForm.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input} from 'antd'
+import { Button, Checkbox, Form, Input, DatePicker} from 'antd'
+import {useState} from 'react';
 
 
 
 function SignForm() {
+    const [attendees, SetAttendees] = useState([])
     
     const onFinish = (values) => {
-    console.log('Received values of form: ', values);
+        let user = {
+            name: values.firstname + " " + values.lastname,
+            email: values.email,
+            password: values.password,
+            birthday: values["birthday"].format("YYYY-MM-DD"),
+        }
+        fetch("http://127.0.0.1:5555/attendees",{
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(user),
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    console.log('Received values of form: ', user);
   };
+
+
 
     return (
         <Form
@@ -18,20 +35,45 @@ function SignForm() {
       onFinish={onFinish}
     >
       <Form.Item
-        name="username"
-        rules={[{ required: true, message: 'Please input your Username!' }]}
+        name="firstname"
+        rules={[{ required: true, message: 'First name required' }]}
+        style={{ display: 'inline-block', width: 'calc(50% - 8px)' }}
       >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+        <Input placeholder="First Name" />
+      </Form.Item>
+      <Form.Item
+        name="lastname"
+        rules={[{ required: true, message: 'Last name required' }]}
+        style={{ display: 'inline-block', width: 'calc(50% - 8px)', margin: '0 8px' }}
+      >
+        <Input placeholder="Last Name" />
+      </Form.Item>
+      <Form.Item
+        name="email"
+        rules={[{ required: true, message: 'Email required' }]}
+      >
+        <Input
+          type="email"
+          placeholder="Email"
+        />
       </Form.Item>
       <Form.Item
         name="password"
-        rules={[{ required: true, message: 'Please input your Password!' }]}
+        rules={[{ required: true, message: 'Password required' },
+        {min: 12, message: 'Password must be minimum 12 characters' }]}
       >
         <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
           type="password"
           placeholder="Password"
         />
+      </Form.Item>
+      <Form.Item name="birthday">
+          <DatePicker 
+          type = "birthday"
+          rules = {[{required: true, message: "Birth date is required"}]}
+          style = {{width: 'calc(100%)'}}
+          format="YYYY-MM-DD"
+          />
       </Form.Item>
       <Form.Item>
         <Form.Item name="remember" valuePropName="checked" noStyle>
@@ -45,9 +87,9 @@ function SignForm() {
 
       <Form.Item>
         <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
+          Sign up
         </Button>
-        Or <a href="">register now!</a>
+        Or <a href="">Have an exising account?</a>
       </Form.Item>
     </Form>
   );
