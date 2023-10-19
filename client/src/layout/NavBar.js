b092dd9e0151e844b54014183bbc32ce4c57a3a1
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-10-19 14:59:46 -0400

    adds my account to nav bar (no redirect to login yet)

--- client/src/layout/NavBar.js
+++ client/src/layout/NavBar.js
@@ -5,8 +5,9 @@ function Navbar() {
   return (
     <div className="navbar">
       <Link to="/contact-page">Contact Us</Link>
-      <Link to="/signup">Signup</Link>
+      <Link to="/signup">Sign Up</Link>
       <Link to="/login">Log In</Link>
+      <Link to="/acctdash"> My Account</Link>
       <Link to="/">Home</Link>
     </div>
   );
