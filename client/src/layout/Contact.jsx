98eb82b3484193463f55b0d6a1b3804060317a08
Sergio Heredia <sergioheredia1996@gmail.com>
2023-10-19 09:26:18 -0700

    integrated main branch and adding event form

--- client/src/layout/Contact.jsx
+++ client/src/layout/Contact.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import EventForm from "../components/EventForm";
 
 function Contact() {
 
     return (
-        <h1>Contact page!</h1>
+        // <h1>Contact page!</h1>
+        <div>
+            <h1>hello</h1>
+            <EventForm/>
+        </div>
     )
 }
 
