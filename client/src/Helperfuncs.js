70ee2c242bd17bcbd75682d2e9651b1c31ceee60
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-10-19 15:10:49 -0400

    adds ternary to navbar to redirect to either login or my account

---
+++ client/src/Helperfuncs.js
@@ -0,0 +1,3 @@
+export function getCurrUser() {
+  return window.sessionStorage.getItem("currentUser");
+}
--- client/src/layout/NavBar.js
+++ client/src/layout/NavBar.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { getCurrUser } from "../Helperfuncs";
 
 function Navbar() {
+  const isLoggedIn = !!getCurrUser();
   return (
     <div className="navbar">
       <Link to="/contact-page">Contact Us</Link>
       <Link to="/signup">Sign Up</Link>
-      <Link to="/login">Log In</Link>
-      <Link to="/acctdash"> My Account</Link>
+      {!isLoggedIn ? (
+        <Link to="/login">Log In</Link>
+      ) : (
+        <Link to="/acctdash"> My Account</Link>
+      )}
       <Link to="/">Home</Link>
     </div>
   );
